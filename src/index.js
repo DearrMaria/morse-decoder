cfcefc7fbe50fb11cbb06c66b81a418871fd03c8
Maria <106482989+DearrMaria@users.noreply.github.com>
2022-07-22 22:24:16 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let str = '';
+    const length = 10;
+    const chapt = 2;
+    
+    for (let m = 0; m < expr.length; m += length) {
+        let letter = expr.slice(m, m + length);
+        let morse = '';
+    
+        if (letter === '**********') {
+          str += ' ';
+        };
+        for(let n = 0; n < letter.length; n += chapt){
+          let symbol = letter.slice(n, n + chapt);
+          switch(symbol){
+            case '10': morse += '.'; break;
+            case '11': morse += '-'; break;
+          }
+        }
+        for (let item in MORSE_TABLE) {
+          if (item === morse) {
+            str += MORSE_TABLE[item];
+          }
+        }
+      }
+    
+      return str;
+    }
 
 module.exports = {
     decode
